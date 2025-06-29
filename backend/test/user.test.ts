8b8acdac33579539853558e415dd981b67475c70
Ray <rayhidayat220@gmail.com>
2025-06-29 23:47:11 +0700

    be: create test utils and test API for user login

--- backend/test/user.test.ts
+++ backend/test/user.test.ts
@@ -1,16 +1,12 @@
-import { afterEach, describe, expect, it } from "@jest/globals";
+import { afterEach, beforeEach, describe, expect, it } from "@jest/globals";
 import { web } from "../src/applications/web";
 import supertest from "supertest";
-import prisma from "../src/applications/database";
 import { logger } from "../src/applications/logger";
+import { createTestUser, removeTestUser } from "./utils";
 
 describe("POST /api/users", () => {
   afterEach(async () => {
-    await prisma.user.deleteMany({
-      where: {
-        username: "rayzor",
-      },
-    });
+    await removeTestUser();
   });
 
   it("should can register new user", async () => {
@@ -21,7 +17,7 @@ describe("POST /api/users", () => {
     });
 
     logger.info(result.body);
-    
+
     expect(result.status).toBe(200);
     expect(result.body.data.username).toBe("rayzor");
     expect(result.body.data.name).toBe("Rayzordev");
@@ -64,6 +60,53 @@ describe("POST /api/users", () => {
     logger.info(result.body);
 
     expect(result.status).toBe(400);
+    expect(result.body.errors).toBeDefined();
+  });
+});
+
+describe("POST /api/users/login", () => {
+  beforeEach(async () => {
+    await createTestUser();
+  });
+
+  afterEach(async () => {
+    await removeTestUser();
+  });
+
+  it("should can login", async () => {
+    const result = await supertest(web).post("/api/users/login").send({
+      username: "rayzor",
+      password: "secret",
+    });
+
+    logger.info(result.body, "token");
+
+    expect(result.status).toBe(200);
+    expect(result.body.data.token).toBeDefined();
+    expect(result.body.data.token).not.toBe("token");
+  });
+
+  it("should reject if request body is invalid", async () => {
+    const result = await supertest(web).post("/api/users/login").send({
+      username: "",
+      password: "",
+    });
+
+    logger.info(result.body);
+
+    expect(result.status).toBe(400);
+    expect(result.body.errors).toBeDefined();
+  });
+
+  it("should reject if username or password wrong", async () => {
+    const result = await supertest(web).post("/api/users/login").send({
+      username: "rayzor",
+      password: "wrong",
+    });
+
+    logger.info(result.body);
+
+    expect(result.status).toBe(401);
     expect(result.body.errors).toBeDefined();
   });
 });
---
+++ backend/test/utils.ts
@@ -0,0 +1,21 @@
+import prisma from "../src/applications/database";
+import bcrypt from "bcrypt";
+
+export const removeTestUser = async () => {
+  await prisma.user.deleteMany({
+    where: {
+      username: "rayzor",
+    },
+  });
+};
+
+export const createTestUser = async () => {
+  await prisma.user.create({
+    data: {
+      username: "rayzor",
+      password: await bcrypt.hash("secret", 10),
+      name: "Rayzordev",
+      token: "token",
+    },
+  });
+};
